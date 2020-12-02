48f88d06218bc3df939827b728c60d6f095411fa
psico <desenvolvedorjg@gmail.com>
2020-12-01 21:34:48 -0300

    [ADD] Old form to import file

---
+++ keyboard-listener.js
@@ -0,0 +1,35 @@
+function createKeyboardListener() {
+    const state = {
+        observers: []
+    }
+
+    function subscribe(observerFunction) {
+        state.observers.push(observerFunction);
+    }
+
+    function notifyAll(command) {
+        console.log(`keyboardListener -> Notifying ${state.observers.length} observers`);
+
+        for (const observerFunction of state.observers) {
+            observerFunction(command);
+        }
+    }
+
+    document.addEventListener('keydown', handleKeydown);
+
+    function handleKeydown(event) {
+        const keyPressed = event.key;
+
+        const command = {
+            playerId: 'player1',
+            keyPressed
+        }
+
+        // game.movePlayer(command);
+        notifyAll(command);
+    }
+
+    return {
+        subscribe
+    }
+}
