4346409b53fc1e4f0537c361c16a72277ab0d829
psico <desenvolvedorjg@gmail.com>
2020-12-11 23:12:36 -0300

    [ADD] exemplo of movement

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ game.addPlayer({playerId: 'player2', playerX: 7, playerY: 0});
 game.addPlayer({playerId: 'player3', playerX: 9, playerY: 0});
 game.addFruit({fruitId: 'fruit1', fruitX: 3, fruitY: 3});
 game.addFruit({fruitId: 'fruit2', fruitX: 3, fruitY: 5});
+game.movePlayer({ playerId: "player1", keyPressed: "ArrowRight"})
 
 server.listen(3000, () => {
     console.log('> Server listening on port: 3000');
