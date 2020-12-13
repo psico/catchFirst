b341411c7d28e6dac610e36ea07d6df78117e6b1
psico <desenvolvedorjg@gmail.com>
2020-12-13 18:25:53 -0300

    [ADD] starting socket.io server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import http from 'http';
 import createGame from "./public/game.js";
+import socketio from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+const sockets = socketio(server);
 
 app.use(express.static('public'));
 
@@ -14,6 +16,11 @@ game.addPlayer({playerId: 'player3', playerX: 9, playerY: 0});
 game.addFruit({fruitId: 'fruit1', fruitX: 3, fruitY: 3});
 game.addFruit({fruitId: 'fruit2', fruitX: 3, fruitY: 5});
 game.movePlayer({ playerId: "player1", keyPressed: "ArrowRight"})
+
+socketio.on("connection", () => {
+    const playerId = sockets.id
+    console.log(`> Player connected on server with id: ${playerId}`);
+});
 
 server.listen(3000, () => {
     console.log('> Server listening on port: 3000');
