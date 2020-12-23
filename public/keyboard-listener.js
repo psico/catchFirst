42e1eedfcb4e97daedf592321989908c6e9e079c
psico <desenvolvedorjg@gmail.com>
2020-12-23 20:45:37 -0300

    [ADD] add register player id

--- public/keyboard-listener.js
+++ public/keyboard-listener.js
@@ -1,6 +1,11 @@
 export default function createKeyboardListener(document) {
     const state = {
-        observers: []
+        observers: [],
+        playerId: null
+    }
+
+    function registerPlayerId(playerId) {
+        state.playerId = playerId;
     }
 
     function subscribe(observerFunction) {
@@ -21,7 +26,7 @@ export default function createKeyboardListener(document) {
         const keyPressed = event.key;
 
         const command = {
-            playerId: 'player1',
+            playerId: state.playerId,
             keyPressed
         }
 
@@ -30,6 +35,7 @@ export default function createKeyboardListener(document) {
     }
 
     return {
-        subscribe
+        subscribe,
+        registerPlayerId
     }
 }
