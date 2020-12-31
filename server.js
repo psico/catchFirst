975a3f92bec5919869fd1db008ca29535fe7a3fb
psico <desenvolvedorjg@gmail.com>
2020-12-31 11:04:02 -0300

    [UPT] add fruit event emit

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const sockets = socketio(server);
 app.use(express.static('public'));
 
 const game = createGame();
+game.start();
 
 game.subscribe((command) => {
     console.log(`> Emitting ${command.type}`);
